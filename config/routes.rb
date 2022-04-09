2c24a0af2db986cd644dd91c4447e9cfccf1dfdf
Eduardo Roldan <eduroldanisaac@gmail.com>
2022-04-09 19:22:03 +0200

    all less destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #read all the restaurants
+  get "tasks", to: "tasks#tasks"
+  #create a new tast
+  get "tasks/new", to: "tasks#new"
+  #view one task
+  get "tasks/:id", to: "tasks#show", as: :task
+  #create a task
+  post "tasks", to: "tasks#create"
+  #edit task
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id/", to: "tasks#update"
+  #delete task
+  delete "tasks/:id", to: "tasks#destroy"
 end
